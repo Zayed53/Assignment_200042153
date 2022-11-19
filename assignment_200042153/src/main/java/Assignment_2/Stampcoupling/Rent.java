e7158a8711888126ee0310e78a5262793a35fcc9
Zayed Hasan <95176649+potato53@users.noreply.github.com>
2022-11-19 23:07:33 +0600

    Update Rent.java

--- assignment_200042153/src/main/java/Assignment_2/Stampcoupling/Rent.java
+++ assignment_200042153/src/main/java/Assignment_2/Stampcoupling/Rent.java
@@ -3,7 +3,7 @@ package Assignment_2.Stampcoupling;
 public class Rent {
     public String user;
 
-    //this fuction uses instance of another class as method parameter. Therefor Class Rent and house is data coupled
+    //this fuction uses instance of another class as method parameter. Therefor Class Rent and house is stamp coupled
     public void rents(house House){
         if(House.isavailable){
             House.Owner=this.user;
